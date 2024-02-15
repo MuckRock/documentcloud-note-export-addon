5457fa734fbb104c8350f3d675e476a1c9f34b42
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2024-02-15 10:13:01 -0600

    Pylint and black

--- main.py
+++ main.py
@@ -3,24 +3,24 @@ This add-on allows you to bulk export notes from DocumentCloud
 """
 
 import zipfile
-
 from documentcloud.addon import AddOn
 
 
 class NoteExport(AddOn):
     """Export all of the selected documents' notes as text in a zip file"""
-    if self.get_document_count() is None:
-            self.set_message("Please select at least one document.")
-            return
+
     def main(self):
+        """ Export all notes as a zip file"""
         with zipfile.ZipFile("export.zip", mode="w") as archive:
             for document in self.client.documents.list(id__in=self.documents):
-                with archive.open(f"{document.slug} - {document.id} - notes.txt", "w") as notes_file:
+                with archive.open(
+                    f"{document.slug} - {document.id} - notes.txt", "w"
+                ) as notes_file:
                     for note in document.notes:
                         notes_file.write(f"{note.title}\n".encode("utf8"))
                         notes_file.write(f"{note.content}\n\n".encode("utf8"))
 
-        self.upload_file(open("export.zip"))
+        self.upload_file(open("export.zip", encoding="utf-8"))
 
 
 if __name__ == "__main__":
